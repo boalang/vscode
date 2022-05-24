208d7f664a20b7ebfc84aac96cc599892629ad06
Robert Dyer <rdyer@unl.edu>
2022-05-24 10:17:56 -0500

    rename variables so they are more descriptive

--- src/linkproviders.ts
+++ src/linkproviders.ts
@@ -61,28 +61,28 @@ export class StudyConfigJSONLinkProvider implements vscode.DocumentLinkProvider 
         }
 
         // looks for queries, e.g.: "query": "queries/hashes.boa",
-        for (const output of document.getText().matchAll(/"query":\s*"([^"]+\.boa)"/g)) {
-            const startPos = output.index + output[0].length - output[1].length - 1;
-            const range = new vscode.Range(document.positionAt(startPos), document.positionAt(startPos + output[1].length));
-            const link = new vscode.DocumentLink(range, vscode.Uri.parse(`file://${jsonPath}/${scriptPath}/${output[1]}`));
-            link.tooltip = `View the Boa query '${scriptPath}/${output[1]}'`;
+        for (const query of document.getText().matchAll(/"query":\s*"([^"]+\.boa)"/g)) {
+            const startPos = query.index + query[0].length - query[1].length - 1;
+            const range = new vscode.Range(document.positionAt(startPos), document.positionAt(startPos + query[1].length));
+            const link = new vscode.DocumentLink(range, vscode.Uri.parse(`file://${jsonPath}/${scriptPath}/${query[1]}`));
+            link.tooltip = `View the Boa query '${scriptPath}/${query[1]}'`;
             links.push(link);
         }
 
         // looks for CSV files, e.g.: "csv": "kotlin/dupes.csv"
-        for (const output of document.getText().matchAll(/"([^"]+\.csv)"/g)) {
-            const range = new vscode.Range(document.positionAt(output.index + 1), document.positionAt(output.index + 1 + output[1].length));
-            const link = new vscode.DocumentLink(range, vscode.Uri.parse(`file://${jsonPath}/${csvPath}/${output[1]}`));
-            link.tooltip = `View the Boa output CSV file '${csvPath}/${output[1]}'`;
+        for (const csv of document.getText().matchAll(/"([^"]+\.csv)"/g)) {
+            const range = new vscode.Range(document.positionAt(csv.index + 1), document.positionAt(csv.index + 1 + csv[1].length));
+            const link = new vscode.DocumentLink(range, vscode.Uri.parse(`file://${jsonPath}/${csvPath}/${csv[1]}`));
+            link.tooltip = `View the Boa output CSV file '${csvPath}/${csv[1]}'`;
             links.push(link);
         }
 
         // looks for replacement files, e.g.: "file": "escape.boa"
-        for (const output of document.getText().matchAll(/"file":\s*"([^"]+\.boa)"/g)) {
-            const startPos = output.index + output[0].length - output[1].length - 1;
-            const range = new vscode.Range(document.positionAt(startPos), document.positionAt(startPos + output[1].length));
-            const link = new vscode.DocumentLink(range, vscode.Uri.parse(`file://${jsonPath}/${snippetPath}/${output[1]}`));
-            link.tooltip = `View the Boa query output file '${snippetPath}/${output[1]}'`;
+        for (const replacement of document.getText().matchAll(/"file":\s*"([^"]+\.boa)"/g)) {
+            const startPos = replacement.index + replacement[0].length - replacement[1].length - 1;
+            const range = new vscode.Range(document.positionAt(startPos), document.positionAt(startPos + replacement[1].length));
+            const link = new vscode.DocumentLink(range, vscode.Uri.parse(`file://${jsonPath}/${snippetPath}/${replacement[1]}`));
+            link.tooltip = `View the Boa query output file '${snippetPath}/${replacement[1]}'`;
             links.push(link);
         }
 

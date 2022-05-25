b1d16130e9c1b43eb29449f8f7ce605609e6201f
Robert Dyer <rdyer@unl.edu>
2022-05-24 22:32:06 -0500

    scope completions to "datasets" key

--- src/studytemplate.ts
+++ src/studytemplate.ts
@@ -23,11 +23,24 @@ import * as consts from './consts';
  */
 class StudyConfigCompletionItemProvider implements vscode.CompletionItemProvider {
     public provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken): vscode.ProviderResult<vscode.CompletionItem[]> {
-        return getDatasets().then((datasets) => {
-            const items = datasets.map((ds) => new vscode.CompletionItem(ds, vscode.CompletionItemKind.Text));
-            items.forEach((item) => item.insertText = '"' + (item.label as string).replace(consts.adminPrefix, '') + '"');
-            return Promise.resolve(items);
-        });
+        if (position.character >= 0) {
+            // scope completions to just the "datasets" key
+            const prefix = document.getText(new vscode.Range(new vscode.Position(0, 0), position));
+            const bracePos = prefix.lastIndexOf('{', prefix.length);
+
+            if (bracePos > -1) {
+                const matches = prefix.match(/"datasets"\s*:\s*{/);
+                if (matches && matches.index + matches[0].length == bracePos + 1) {
+                    return getDatasets().then((datasets) => {
+                        const items = datasets.map((ds) => new vscode.CompletionItem(ds, vscode.CompletionItemKind.Text));
+                        items.forEach((item) => item.insertText = '"' + (item.label as string).replace(consts.adminPrefix, '') + '"');
+                        return Promise.resolve(items);
+                    });
+                }
+            }
+        }
+
+        return [];
     }
 }
 

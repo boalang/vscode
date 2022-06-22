289ab1f7fe8359f05bf8be4bec627d185579530b
Robert Dyer <rdyer@unl.edu>
2022-06-22 10:44:46 -0500

    better handling of template autocompletes

--- src/studytemplate/BoaCompletionItemProvider.ts
+++ src/studytemplate/BoaCompletionItemProvider.ts
@@ -22,39 +22,45 @@ export default class BoaCompletionItemProvider implements vscode.CompletionItemP
     public async provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken): Promise<vscode.CompletionItem[]> {
         const items = [];
 
-        if (position.character >= 0) {
-            const substitutions = cache.getSubstitutions();
-            let keys = Object.keys(substitutions.substitutions);
-            const hovers = new Map<string,string[]>();
+        const match = document.getWordRangeAtPosition(position, /{@[^@]*(\s*@})?/);
+        const tags = document.getText(match);
+        const hasStartTag = tags.startsWith("{@");
+        const hasEndTag = tags.endsWith("@}");
 
-            for (const k of keys) {
-                if (!hovers.has(k)) hovers.set(k, []);
-                const sub = substitutions.substitutions[k];
-                hovers.get(k).push(await cache.renderSubstitution(sub.subst, sub.output));
-            }
+        const substitutions = cache.getSubstitutions();
+        let keys = Object.keys(substitutions.substitutions);
+        const hovers = new Map<string,string[]>();
 
-            for (const k of Object.keys(substitutions).filter(filename => document.fileName.endsWith(filename))) {
-                const newKeys = Object.keys(substitutions[k]);
-                for (const k2 of newKeys) {
-                    if (!hovers.has(k2)) hovers.set(k2, []);
-                    const sub = substitutions[k][k2];
-                    hovers.get(k2).push(await cache.renderSubstitution(sub.subst, sub.output));
-                }
-                keys = keys.concat(newKeys);
-            }
-
-            removeDuplicates(keys).forEach(k => {
-                items.push(this.makeCompletionItem(k, hovers.get(k).reverse()));
-            });
+        for (const k of keys) {
+            if (!hovers.has(k)) hovers.set(k, []);
+            const sub = substitutions.substitutions[k];
+            hovers.get(k).push(await cache.renderSubstitution(sub.subst, sub.output));
         }
+
+        for (const k of Object.keys(substitutions).filter(filename => document.fileName.endsWith(filename))) {
+            const newKeys = Object.keys(substitutions[k]);
+            for (const k2 of newKeys) {
+                if (!hovers.has(k2)) hovers.set(k2, []);
+                const sub = substitutions[k][k2];
+                hovers.get(k2).push(await cache.renderSubstitution(sub.subst, sub.output));
+            }
+            keys = keys.concat(newKeys);
+        }
+
+        removeDuplicates(keys).forEach(k => {
+            items.push(this.makeCompletionItem(k, hasStartTag, hasEndTag, hovers.get(k).reverse()));
+        });
 
         return items;
     }
 
-    private makeCompletionItem(label: string, hovers: string[]) {
+    private makeCompletionItem(label: string, hasStartTag, hasEndTag, hovers: string[]) {
         const trimmed = label.substring(2, label.length - 2);
         const item = new vscode.CompletionItem(trimmed, vscode.CompletionItemKind.Value);
         item.detail = 'substitution';
+        if (!hasStartTag || !hasEndTag) {
+            item.insertText = label.substring(hasStartTag ? 2 : 0, hasEndTag ? -2 : label.length);
+        }
         if (hovers) {
             item.documentation = new vscode.MarkdownString(hovers.join('\n\n----\n\n'), true);
         }

6ed1a32ddf2e56d4c117812e7cadb03471ec591c
Robert Dyer <rdyer@unl.edu>
2022-07-08 01:38:02 -0500

    when cancelling, return what we have already

--- src/completions.ts
+++ src/completions.ts
@@ -23,7 +23,7 @@ export default class BuiltInFunctionsCompletionItemProvider implements vscode.Co
         const items = [];
 
         for (const c of Object.keys(builtinConsts)) {
-            if (token.isCancellationRequested) return [];
+            if (token.isCancellationRequested) return items;
             const item = new vscode.CompletionItem(c, vscode.CompletionItemKind.Value);
             item.detail = c;
             if (builtinConsts[c].length > 0)
@@ -32,7 +32,7 @@ export default class BuiltInFunctionsCompletionItemProvider implements vscode.Co
         }
 
         for (const v of Object.keys(builtinVars)) {
-            if (token.isCancellationRequested) return [];
+            if (token.isCancellationRequested) return items;
             const item = new vscode.CompletionItem(v, vscode.CompletionItemKind.Variable);
             item.detail = `(var) ${v}: ${builtinVars[v].type}`;
             if (builtinVars[v].help.length > 0)
@@ -41,7 +41,7 @@ export default class BuiltInFunctionsCompletionItemProvider implements vscode.Co
         }
 
         for (const funcName of Object.keys(builtinFunctions)) {
-            if (token.isCancellationRequested) return [];
+            if (token.isCancellationRequested) return items;
             const item = new vscode.CompletionItem(funcName, vscode.CompletionItemKind.Function);
             item.detail = getFuncSignature(funcName);
             item.documentation = new vscode.MarkdownString(getFuncDoc(funcName));
@@ -49,7 +49,7 @@ export default class BuiltInFunctionsCompletionItemProvider implements vscode.Co
         }
 
         for (const typeName of Object.keys(builtinTypes)) {
-            if (token.isCancellationRequested) return [];
+            if (token.isCancellationRequested) return items;
             const item = new vscode.CompletionItem(typeName, vscode.CompletionItemKind.Function);
             item.detail = `(type) ${typeName}`;
             item.documentation = new vscode.MarkdownString(builtinTypes[typeName].help);

d03d090ae5afe11d89e9a0290c1f74c94ccfda26
Robert Dyer <rdyer@unl.edu>
2022-07-16 13:51:21 -0500

    finalize the extract snippet refactoring

--- src/studytemplate/refactor.ts
+++ src/studytemplate/refactor.ts
@@ -18,6 +18,7 @@ import * as vscode from 'vscode';
 import { TextEncoder } from 'util';
 import * as consts from '../consts';
 import { getWorkspaceRoot } from '../utils';
+import { cache } from './jsoncache';
 
 export class BoaTemplateRefactoringProvider implements vscode.CodeActionProvider {
     async provideCodeActions(document: vscode.TextDocument, range: vscode.Range | vscode.Selection, context: vscode.CodeActionContext, token: vscode.CancellationToken): Promise<vscode.CodeAction[]> {
@@ -49,21 +50,55 @@ export class BoaTemplateRefactoringProvider implements vscode.CodeActionProvider
 export async function extractSnippet(document: vscode.TextDocument, range: vscode.Range) {
     const text = document.getText(range);
 
-    let path = getWorkspaceRoot() + '/' + consts.snippetPath;
+    let path = getWorkspaceRoot() + '/' + consts.snippetPath + '/';
     vscode.workspace.fs.createDirectory(vscode.Uri.file(path));
 
-    // FIXME make sure filename is unique
-    path += '/' + 'new-snippet.boa';
-    await vscode.workspace.fs.writeFile(vscode.Uri.file(path), new TextEncoder().encode(text));
+    // make sure filename is unique
+    let filename = 'new-snippet';
+    let exists = 1;
+    while (exists !== 0) {
+        try {
+            let stat = await vscode.workspace.fs.stat(vscode.Uri.file(path + filename + '.boa'));
+            filename = 'new-snippet' + exists++;
+        } catch (e) {
+            exists = 0;
+        }
+    }
 
-    // TODO update study config to add new substitution
+    await vscode.workspace.fs.writeFile(vscode.Uri.file(path + filename + '.boa'), new TextEncoder().encode(text));
 
+    // make sure template name is unique
+    const substitutions = cache.getSubstitutions();
+    let alltags = Object.keys(substitutions.substitutions);
+
+    for (const filename in substitutions) {
+        if (filename != 'substitutions' && document.fileName.endsWith(filename)) {
+            for (const items of substitutions[filename]) {
+                alltags = alltags.concat(Object.keys(items));
+            }
+        }
+    }
+
+    let template = 'new-snippet';
+    exists = 1;
+    while (exists !== 0) {
+        if (alltags.indexOf(`{@${template}@}`) === -1) {
+            exists = 0;
+        } else {
+            template = 'new-snippet' + exists++;
+        }
+    }
+    template = `{@${template}@}`;
+
+    // update study config to add new tag
+    cache.addTemplateTag(template);
+
+    // edit the original document
     const edit = new vscode.WorkspaceEdit();
-    // FIXME make sure template name is unique
-    const template = '{@new-snippet@}';
     edit.replace(document.uri, range, template);
     await vscode.workspace.applyEdit(edit);
 
+    // perform a rename to allow the user to select a tag name
     vscode.window.activeTextEditor.selection = new vscode.Selection(range.start, new vscode.Position(range.start.line, range.start.character + template.length));
     await vscode.commands.executeCommand('editor.action.rename', [
         document.uri,

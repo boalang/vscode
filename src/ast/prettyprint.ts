d33d93184c6c602d13e58bb52209127764ddc760
Robert Dyer <rdyer@unl.edu>
2023-01-08 15:35:30 -0600

    remove fixme that is fixed

--- src/ast/prettyprint.ts
+++ src/ast/prettyprint.ts
@@ -309,7 +309,6 @@ export default class PrettyPrinter extends AbstractParseTreeVisitor<string> impl
 
     visitForStatement(ctx: ast.ForStatementContext) {
         const body = ctx.programStatement();
-        // FIXME this does not work - we cant tell which case a single forExpression() is
         return 'for (' + ctx.forExpression()?.accept(this) + '; ' + ctx.expression()?.accept(this) + '; ' + ctx.forExpressionStatement()?.accept(this) + ')' + this.openStmtIsBlock(ctx.RPAREN(), body) +
                 body.accept(this) +
             this.closeStmtIsBlock(body);
